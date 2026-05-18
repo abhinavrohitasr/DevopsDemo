ff69ffbd295e56c137c7337f8a715dc6fd909649
Abhinav Rohit <abhinavkumarrohit@gmail.com>
2026-05-19 00:20:00 +0530

    Initial commit for helloworld Program

---
+++ helloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+	System.out.println("Hello world File written in Local and pushed to github");
+        System.out.println("Hello World from Jenkins + Docker!");
+	System.out.println("Abhinav Rohit");
+    }
+}
