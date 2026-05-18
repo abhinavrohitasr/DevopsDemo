c11f5f671211f54e16dbb8ba60a2a8599b6ae76b
Abhinav Kumar <59612887+abhinavrohitasr@users.noreply.github.com>
2026-05-19 01:26:26 +0530

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class helloWorld {
     public static void main(String[] args) {
 	System.out.println("Hello world File written in Local and pushed to github");
         System.out.println("Hello World from Jenkins + Docker!");
